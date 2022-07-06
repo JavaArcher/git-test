bd354c1916419491a43363a6f96a7de1edce3e41
admin <admin@qq.com>
2022-07-06 19:22:16 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author qianlei
+ * @since 2022/7/6 下午 07:18
+ * TODO
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello , git");
+    }
+}
